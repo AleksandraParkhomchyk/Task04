2caf6cafd0a145349615d14e652f078aea8761c5
Alex <aparhomchyk@gmail.com>
2022-01-11 18:59:47 +0300

    Account

---
+++ src/main/java/com/epam/tr/task04/paymentsapp/controller/command_impl/CreateAccountCommand.java
@@ -0,0 +1,36 @@
+package com.epam.tr.task04.paymentsapp.controller.command_impl;
+
+import com.epam.tr.task04.paymentsapp.controller.Command;
+import com.epam.tr.task04.paymentsapp.services.AccountService;
+import com.epam.tr.task04.paymentsapp.services.ServiceFactory;
+import com.epam.tr.task04.paymentsapp.services.UserService;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class CreateAccountCommand implements Command {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        ServiceFactory serviceFactory = ServiceFactory.getInstance();
+        AccountService accountService = serviceFactory.getAccountService();
+
+
+        boolean result = accountService.createAccount();
+
+       // HttpSession session = request.getSession(true);
+
+        if (!result) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/userPage.jsp");
+            dispatcher.forward(request, response);
+            System.out.println("Счет не создан");
+        } else {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/accountCreated.jsp");
+            dispatcher.forward(request, response);
+            System.out.println("Счет создан");
+        }
+    }
+}
