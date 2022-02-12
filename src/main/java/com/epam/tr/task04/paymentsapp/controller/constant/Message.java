b33cb1232a6c99bfe41a3ea3a69f16b802acd62a
Alex <aparhomchyk@gmail.com>
2022-02-12 19:24:25 +0300

    Message fixes

--- src/main/java/com/epam/tr/task04/paymentsapp/controller/constant/Message.java
+++ src/main/java/com/epam/tr/task04/paymentsapp/controller/constant/Message.java
@@ -2,16 +2,16 @@ package com.epam.tr.task04.paymentsapp.controller.constant;
 
 public class Message {
 
-    public final static String MESSAGE = "message";
-    public final static String WRONG_LOGIN_PASSWORD = "Wrong login or password";
-    public final static String SUCCESS_PAYMENT = "Payment made successful";
-    public final static String FAILURE_PAYMENT = "You have insufficient funds";
-    public final static String SUCCESS_CASHOUT = "Cashout request was made successful";
-    public final static String FAILURE_CASHOUT = "You have insufficient funds";
-    public final static String CREATE_ACCOUNT = "Please, create an account.";
-    public final static String ACCOUNT_CREATED = "Your account created successful";
-    public final static String ACCOUNT_EXIST = "You have an account already";
-    public final static String ACCOUNT_BLOCKED = "Your account is blocked. Please contact your admin";
-    public final static String INVALID_ACCOUNT_OR_AMOUNT = "Invalid target account number or amount";
-    public final static String INVALID_AMOUNT = "Invalid amount";
+    public static final String MESSAGE = "message";
+    public static final String WRONG_LOGIN_PASSWORD = "Wrong login or password";
+    public static final String SUCCESS_PAYMENT = "Payment made successful";
+    public static final String FAILURE_PAYMENT = "You have insufficient funds";
+    public static final String SUCCESS_CASHOUT = "Cashout request was made successful";
+    public static final String FAILURE_CASHOUT = "You have insufficient funds";
+    public static final String CREATE_ACCOUNT = "Please, create an account.";
+    public static final String ACCOUNT_CREATED = "Your account created successful";
+    public static final String ACCOUNT_EXIST = "You have an account already";
+    public static final String ACCOUNT_BLOCKED = "Your account is blocked. Please contact your admin";
+    public static final String INVALID_ACCOUNT_OR_AMOUNT = "Invalid target account number or amount";
+    public static final String INVALID_AMOUNT = "Invalid amount";
 }
