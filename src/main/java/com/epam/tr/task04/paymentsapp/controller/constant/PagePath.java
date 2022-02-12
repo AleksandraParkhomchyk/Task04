93e33993e51d1044b4fa53069af38d992e7311d0
Alex <aparhomchyk@gmail.com>
2022-02-12 19:26:30 +0300

    PagePath fixes

--- src/main/java/com/epam/tr/task04/paymentsapp/controller/constant/PagePath.java
+++ src/main/java/com/epam/tr/task04/paymentsapp/controller/constant/PagePath.java
@@ -2,18 +2,18 @@ package com.epam.tr.task04.paymentsapp.controller.constant;
 
 public final class PagePath {
 
-    public final static String HOME_PAGE = "WEB-INF/jsp/homePage.jsp";
-    public final static String USER_PAGE = "WEB-INF/jsp/userPage.jsp";
-    public final static String ADMIN_PAGE = "WEB-INF/jsp/adminPage.jsp";
-    public final static String ERROR_PAGE = "WEB-INF/jsp/errorPage.jsp";
-    public final static String LOGINATION_PAGE = "WEB-INF/jsp/logination.jsp";
-    public final static String REGISTRATION_PAGE = "WEB-INF/jsp/registration.jsp";
-    public final static String INDEX_PAGE = "index.jsp";
-    public final static String HISTORY_PAGE = "WEB-INF/jsp/historyPage.jsp";
-    public final static String REQUESTS_PAGE = "WEB-INF/jsp/requestsPage.jsp";
+    public static final String HOME_PAGE = "WEB-INF/jsp/homePage.jsp";
+    public static final String USER_PAGE = "WEB-INF/jsp/userPage.jsp";
+    public static final String ADMIN_PAGE = "WEB-INF/jsp/adminPage.jsp";
+    public static final String ERROR_PAGE = "WEB-INF/jsp/errorPage.jsp";
+    public static final String LOGINATION_PAGE = "WEB-INF/jsp/logination.jsp";
+    public static final String REGISTRATION_PAGE = "WEB-INF/jsp/registration.jsp";
+    public static final String INDEX_PAGE = "index.jsp";
+    public static final String HISTORY_PAGE = "WEB-INF/jsp/historyPage.jsp";
+    public static final String REQUESTS_PAGE = "WEB-INF/jsp/requestsPage.jsp";
 
-    public final static String URL_LOGINATION = "/payments/controller?command=GO_TO_LOGINATION_PAGE";
-    public final static String URL_USERS_PAGE = "/payments/controller?command=GO_TO_USERS_PAGE";
+    public static final String URL_LOGINATION = "/payments/controller?command=GO_TO_LOGINATION_PAGE";
+    public static final String URL_USERS_PAGE = "/payments/controller?command=GO_TO_USERS_PAGE";
 
 
 }
