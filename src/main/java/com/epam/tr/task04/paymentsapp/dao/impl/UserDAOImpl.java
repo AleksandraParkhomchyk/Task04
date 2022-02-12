ae76aa4efce4d691ae42803fdcb2189a9c80bdad
Alex <aparhomchyk@gmail.com>
2022-02-12 19:05:30 +0300

    UserDAOImpl fixes

--- src/main/java/com/epam/tr/task04/paymentsapp/dao/impl/UserDAOImpl.java
+++ src/main/java/com/epam/tr/task04/paymentsapp/dao/impl/UserDAOImpl.java
@@ -12,8 +12,8 @@ import java.util.Optional;
 
 public class UserDAOImpl implements UserDAO {
 
-    private final String saveUser = "INSERT INTO users(u_name, u_surname, u_login, u_password, u_passport, roles_r_id) VALUES( ?, ?, ?, ?, ?, ?)";
-    private final String authorisation = "SELECT u_id, u_login, u_password, roles_r_id FROM users WHERE u_login = ?";
+    private static final String SAVE_USER = "INSERT INTO users(u_name, u_surname, u_login, u_password, u_passport, roles_r_id) VALUES( ?, ?, ?, ?, ?, ?)";
+    private static final String AUTHORISATION = "SELECT u_id, u_login, u_password, roles_r_id FROM users WHERE u_login = ?";
 
     private static final String PASSWORD_SALT = "$2a$10$7Xtwz2dUaNW2055I9dhhv.";
 
@@ -21,14 +21,14 @@ public class UserDAOImpl implements UserDAO {
     public void saveUser(User user) throws DAOException {
 
         try (Connection connection = ConnectionPool.getInstance().takeConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(saveUser)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_USER)) {
 
-            String password_hash = BCrypt.hashpw(user.getPassword(), PASSWORD_SALT);
+            String passwordHash = BCrypt.hashpw(user.getPassword(), PASSWORD_SALT);
 
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getSurname());
             preparedStatement.setString(3, user.getLogin());
-            preparedStatement.setString(4, password_hash);
+            preparedStatement.setString(4, passwordHash);
             preparedStatement.setString(5, user.getPassport());
             preparedStatement.setInt(6, 1);
 
@@ -44,7 +44,7 @@ public class UserDAOImpl implements UserDAO {
         User user = null;
 
         try (Connection connection = ConnectionPool.getInstance().takeConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(authorisation)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(AUTHORISATION)) {
 
             preparedStatement.setString(1, login);
 
