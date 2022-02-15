5c33c312836540576e553fcf6ef5a0477a8d0cee
Alex <aparhomchyk@gmail.com>
2022-02-15 23:02:11 +0300

    javadocs

--- src/main/java/com/epam/tr/task04/paymentsapp/service/CashRequestService.java
+++ src/main/java/com/epam/tr/task04/paymentsapp/service/CashRequestService.java
@@ -8,19 +8,75 @@ import com.epam.tr.task04.paymentsapp.service.validator.ValidatorException;
 
 import java.util.List;
 
+/**
+ * Cashrequest service interface
+ */
 public interface CashRequestService {
 
+    /**
+     * Create cashoutrequest
+     *
+     * @param account account
+     * @param amount  cashoutrequest amount
+     * @return cashoutrequest entity if successfully created
+     * @throws ServiceException           when dao exception occurs
+     * @throws InsufficientFundsException when  insufficient funds
+     * @throws ValidatorException         when amount data is invalid
+     */
     CashoutRequest cashout(Account account, String amount) throws ServiceException, InsufficientFundsException, ValidatorException;
 
+    /**
+     * Get all pending cashoutrequests
+     *
+     * @return list of pending cashoutrequests
+     * @throws ServiceException when dao exception occurs
+     */
     List<CashoutRequest> getAllCashoutRequests() throws ServiceException;
 
+    /**
+     * Approve cashoutrequest
+     *
+     * @param account   account
+     * @param requestID cashoutrequest id
+     * @param amount    cashoutrequest amount
+     * @param userId    user id
+     * @return if updated successfully
+     * @throws ServiceException when dao exception occurs
+     */
     boolean updateRequestStatusApproved(Account account, Integer requestID, Double amount, Integer userId) throws ServiceException;
 
+    /**
+     * Decline cashoutrequest
+     *
+     * @param requestID cashoutrequest id
+     * @return if updated successfully
+     * @throws ServiceException when dao exception occurs
+     */
     boolean updateRequestStatusDeclined(Integer requestID) throws ServiceException;
 
+    /**
+     * Get amount by cashoutrequest id
+     *
+     * @param requestId cashoutrequest id
+     * @return amount of cashoutrequest
+     * @throws ServiceException when dao exception occurs
+     */
     Double getAmountByRequestId(Integer requestId) throws ServiceException;
 
+    /**
+     * Get users cashoutrequests
+     *
+     * @param accountId account id
+     * @return list of users cashoutrequests
+     * @throws ServiceException when dao exception occurs
+     */
     List<CashoutRequest> getUsersRequests(Integer accountId) throws ServiceException;
 
+    /**
+     * Cancel request
+     *
+     * @param requestId cashoutrequest id
+     * @throws ServiceException when dao exception occurs
+     */
     void cancelCashRequest(Integer requestId) throws ServiceException;
 }
--- src/main/java/com/epam/tr/task04/paymentsapp/service/UserService.java
+++ src/main/java/com/epam/tr/task04/paymentsapp/service/UserService.java
@@ -3,10 +3,32 @@ package com.epam.tr.task04.paymentsapp.service;
 import com.epam.tr.task04.paymentsapp.entity.User;
 import com.epam.tr.task04.paymentsapp.service.exception.ServiceException;
 
+/**
+ * User service interface
+ */
 public interface UserService {
 
+    /**
+     * Login user
+     *
+     * @param login user login
+     * @param password user password
+     * @return user entity if successfully logged in
+     * @throws ServiceException when dao exception occurs
+     */
     User authorisation(String login, String password) throws ServiceException;
 
+    /**
+     * Regiger new user
+     *
+     * @param name user first name
+     * @param surname user surname
+     * @param login user login
+     * @param password user password
+     * @param passport user passport
+     * @return if user successfully registered
+     * @throws ServiceException when dao exception occurs
+     */
     boolean registration(String name, String surname, String login, String password, String passport) throws ServiceException;
 
 }
